30bfcebc42d8e680fb05720a3091969cc5a8d6c3
HebertyRichards <143839840+HebertyRichards@users.noreply.github.com>
2025-02-20 16:56:39 -0300

    Add files via upload

--- SegurançaUrl.php
+++ SegurançaUrl.php
@@ -1,5 +1,5 @@
-  <?php
-  session_start();
+<?php
+session_start();
 
   if (isset($_SESSION['nome'])) {
       $nome_usuario = $_SESSION['nome'];
@@ -156,20 +156,19 @@
   <footer>
     <div class="contato">
       <div class="contato-1">
-        <ul><h1>Contato</h1>
-      <li><img src="imagens/gmail.png">Geeknine@gmail.com</li>
-      <li><img src="imagens/telefone.png">Telefone:(11) 0000-0000</li>
-      <li><img src="imagens/whatsapp.png">Whatsapp: (11) 00000-0000</li>
-    </ul>
+        <ul>
+          <li id="h2">Contato</li>
+          <li><img src="imagens/gmail.png" alt="Gmail">Geeknine@gmail.com</li>
+          <li><img src="imagens/telefone.png" alt="Telefone">Telefone:(11) 0000-0000</li>
+          <li><img src="imagens/whatsapp.png" alt="WhatsApp">Whatsapp: (11) 00000-0000</li>
+        </ul>
+      </div>
+      <div class="redes-sociais">
+        <a id="facebook" href="https://www.facebook.com/people/GeekNine/61557039160232/" target="_blank"></a>
+        <a id="instagram" href="https://www.instagram.com/geek_nine/?igsh=MXBqemduaHFybDFuZg%3D%3D" target="_blank"></a>
+        <a id="twitter" href="https://twitter.com/GeekNine9" target="_blank"></a>
+      </div>
     </div>
-  <div class="redes-sociais"> 
-    <a href="https://www.facebook.com/people/GeekNine/61557039160232/" target="_blank"><button
-        title="Facebook">Facebook</button></a>
-    <a href="https://www.instagram.com/geek_nine/?igsh=MXBqemduaHFybDFuZg%3D%3D" target="_blank"><button
-        title="Instagram">Instagram</button></a>
-    <a href="https://twitter.com/GeekNine9" target="_blank"><button title="Twitter">Twitter</button></a>
-  </div>
-  </div>
   <div class="logo">
   <p>GeekNine © 2024 Todos os Direitos Reservados</p>
   </div>
